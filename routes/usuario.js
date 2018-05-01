be0f853f7ddb056bc7c987b91754102956dc7eca
carlosTangarife <cjt860826@gmail.com>
2018-05-01 13:51:07 -0500

    se hace la primera consulta a la base de datos, y la api en el endpoint usuarios, devuelve los datos correspondientes a esa collecion

--- routes/usuario.js
+++ routes/usuario.js
@@ -4,11 +4,24 @@ var express = require('express');
 // inicializar variables
 var app = express();
 
+var Usaurio = require('../models/usuario')
+
 app.get('/', (req, res) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Get de usaurios'
-    })
+
+    Usaurio.find({}, (err, usuarios)=> {
+        if(err) {
+            return err.status(500).json({
+                ok: false,
+                mensaje: 'Error cargando usuario',
+                error: err
+            })
+        } 
+
+        res.status(200).json({
+            ok: true,
+            usuarios: usuarios
+        })
+    });
 });
 
 module.exports = app;
