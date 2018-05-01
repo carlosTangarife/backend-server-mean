b573c21d841c1781c3603f880deb5e093dc76122
carlosTangarife <cjt860826@gmail.com>
2018-05-01 13:26:15 -0500

    primera conexion con la base de datos

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Requires
 var express = require('express');
+var mongoose = require('mongoose');
 
 // inicializar variables
 var app = express();
+
+// conexion a la base de datos
+mongoose.connect('mongodb://localhost/hospitalDB');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', ()=> console.log('MogoDB: \x1b[36m%s\x1b[0m', 'online'));
 
 // peticion get, primer parametros, request, segundo parametro, la respuesta
 app.get('/', (req, res) => {
@@ -14,6 +21,5 @@ app.get('/', (req, res) => {
 
 // Escucahr peticiones
 app.listen(3000, ()=> {
-    console.log('Node/Express: \x1b[36m%s\x1b[42m', 'online');
-    
+    console.log('Node/Express: \x1b[36m%s\x1b[0m', 'online');  
 })
